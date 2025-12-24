fd12cd2827ce93020382884b5755bfbcab5409d5
NikitaKikita007 <119515103+NikitaKikita007@users.noreply.github.com>
2025-12-24 16:49:45 +0200

    kykysiki

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
-const answer = document.querySelector('.answer');
-answer.addEventListener('click', () => {
-    answer.classList.add('active');
-
-    setTimeout(() => {
-      answer.classList.remove('active');
-    });
+const answer = document.querySelectorAll('.answer');
+answer.forEach((item)=>{
+  item.addEventListener('click', () => {
+      item.classList.add('active');
+  
+      setTimeout(() => {
+        item.classList.remove('active');
+      }, 1000);
+  })
 })
