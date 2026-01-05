21d85063d49cde20e078892855c40e13dd321e65
vlada <vladkanmsk@icloud.com>
2026-01-05 17:11:05 +0200

    js и json

--- jsArtema.js
+++ jsArtema.js
@@ -1,20 +1,17 @@
-const voprosBlock = document.querySelector(".vopros")
-const answersBlock = document.querySelector(".answers")
+const testName = document.querySelector("#test1")
+const testTema = document.querySelector("#tema")
+const testAuthor = document.querySelector("#author")
+const testPriclad = document.querySelector("#priclad")
 async function loadData() {
   try {
     const response = await fetch('./json.json');
     const jsonData = await response.json(); // Parses the response into a JS object
-    console.log(jsonData.questions);
-    let questions = jsonData.questions
-    voprosBlock.textContent = questions[0].question
-    const ans1Block = document.createElement('div')
-    ans1Block.classList.add('answer')
-    ans1Block.textContent = questions[0].answers[0].answers
-    answersBlock.appendChild(ans1Block)
-    for (let i = 0; i < questions.length; i++) {
-      const element = questions[i];
-      console.log(element)
-    }
+    console.log(jsonData);
+    testName.textContent = jsonData.nazvanie
+    testTema.textContent = jsonData.tema
+    testAuthor.textContent = jsonData.author
+    testPriclad.textContent = jsonData.priclad
+
   } catch (error) {
     console.error('Error fetching JSON:', error);
   }
