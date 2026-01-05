1d251657de78f41a536ab421b1cd84d57836a6c1
peeper4 <mishatronic7045@gmail.com>
2026-01-05 17:10:54 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,28 @@
 const voprosBlock = document.querySelector(".vopros")
 const answersBlock = document.querySelector(".answers")
-async function loadData() {
+ 
+  async function loadData() {
   try {
     const response = await fetch('./json.json');
     const jsonData = await response.json(); // Parses the response into a JS object
     console.log(jsonData.questions);
     let questions = jsonData.questions
-    voprosBlock.textContent = questions[0].question
-    const ans1Block = document.createElement('div')
-    ans1Block.classList.add('answer')
-    ans1Block.textContent = questions[0].answers[0].answers
-    answersBlock.appendChild(ans1Block)
     for (let i = 0; i < questions.length; i++) {
+      let answers = jsonData.questions[i].answers
       const element = questions[i];
       console.log(element)
+    voprosBlock.textContent = questions[i].question
+    console.log(answers)
+    for (let j = 0; j < answers.length; j++) {
+      console.log('jopa')
+         const element = answers[j].answers;
+         const ans1Block = document.createElement('div')
+         ans1Block.classList.add('answer')
+         ans1Block.textContent = element
+         answersBlock.appendChild(ans1Block)
+         console.log(element)
+    }
+ 
     }
   } catch (error) {
     console.error('Error fetching JSON:', error);
