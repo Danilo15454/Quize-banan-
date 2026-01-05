a41180c7720535bd7b27a8ec652ca1bd19f7a315
NikitaKikita007 <119515103+NikitaKikita007@users.noreply.github.com>
2026-01-05 16:47:35 +0200

    adding answers

--- script.js
+++ script.js
@@ -1,10 +1,18 @@
+const voprosBlock = document.querySelector(".vopros")
+const answersBlock = document.querySelector(".answers")
 async function loadData() {
   try {
     const response = await fetch('./json.json');
     const jsonData = await response.json(); // Parses the response into a JS object
     console.log(jsonData.questions);
-    for (let i = 0; i < arr.length; i++) {
-      const element = arr[i];
+    let questions = jsonData.questions
+    voprosBlock.textContent = questions[0].question
+    const ans1Block = document.createElement('div')
+    ans1Block.classList.add('answer')
+    ans1Block.textContent = questions[0].answers[0].answers
+    answersBlock.appendChild(ans1Block)
+    for (let i = 0; i < questions.length; i++) {
+      const element = questions[i];
       console.log(element)
     }
   } catch (error) {
