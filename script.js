cfc22a3b5a72a856db324ccebd357578789cabc7
peeper4 <mishatronic7045@gmail.com>
2026-01-07 17:38:46 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,32 +1,38 @@
 const voprosBlock = document.querySelector(".vopros")
 const answersBlock = document.querySelector(".answers")
- 
+let counter = 0
   async function loadData() {
   try {
     const response = await fetch('./json.json');
     const jsonData = await response.json(); // Parses the response into a JS object
     console.log(jsonData.questions);
     let questions = jsonData.questions
-    for (let i = 0; i < questions.length; i++) {
-      let answers = jsonData.questions[i].answers
-      const element = questions[i];
-      console.log(element)
-    voprosBlock.textContent = questions[i].question
-    console.log(answers)
-    for (let j = 0; j < answers.length; j++) {
-      console.log('jopa')
-         const element = answers[j].answers;
-         const ans1Block = document.createElement('div')
-         ans1Block.classList.add('answer')
-         ans1Block.textContent = element
-         answersBlock.appendChild(ans1Block)
-         console.log(element)
-    }
- 
-    }
+  
+  switcher(counter, questions)
   } catch (error) {
     console.error('Error fetching JSON:', error);
   }
 }
 
+
 loadData();
+function switcher(i, questions) {
+    let answers = questions[i].answers
+      const element = questions[i];
+      console.log(element)
+    voprosBlock.textContent = questions[i].question
+    console.log(answers)
+    for (let j = 0; j < answers.length; j++) {
+         const element = answers[j].answers;
+         const ans1Block = document.createElement('div')
+         ans1Block.classList.add('answer')
+         ans1Block.textContent = element
+         answersBlock.appendChild(ans1Block)
+         ans1Block.addEventListener("click", ()=>{
+          counter++
+          console.log("asdsadas")
+          switcher(i, questions)
+         })
+         console.log(element)
+}
+}
