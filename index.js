869307c90aeee6364bd5c280eb00bc237f721ed3
Unitaz <planset980@gmail.com>
2026-01-07 17:09:27 +0200

    234

--- index.js
+++ index.js
@@ -2,11 +2,21 @@ const button1 = document.querySelector('.test1')
 const button2 = document.querySelector('.test2')
 async function firsttest(){
     try {
-        const response = await fetch('./json.json')
-        const jsonData = response.json()
-        console.log(jsonData.questions)
+        const response = await fetch('./json.json');
+        const jsonData = await response.json(); // Parses the response into a JS object
+        console.log(jsonData.questions);
     } catch(error) {
         console.log("eror lol")
+    }
+}
+
+async function sectest(){
+    try {
+        const response = await fetch('./json2.json');
+        const jsonData = await response.json(); // Parses the response into a JS object
+        console.log(jsonData.questions);
+    } catch(error) {
+        console.log("eror lol 1")
     }
 }
 button1.addEventListener('click', () => {
@@ -14,5 +24,5 @@ firsttest()
 })
 
 button2.addEventListener('click', () => {
-
+sectest()
 })
