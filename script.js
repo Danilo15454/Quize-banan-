09656e0db60994db5db9b31673779b6635ae294c
Danila <planset980@gmail.com>
2026-01-14 19:39:05 +0200

    easy lol

--- script.js
+++ script.js
@@ -1,38 +1,41 @@
 const voprosBlock = document.querySelector(".vopros")
 const answersBlock = document.querySelector(".answers")
-let counter = 0
-  async function loadData() {
+const questCounterBlock = document.querySelector(".planka")
+storage = window.localStorage
+let questCounter = 0
+
+async function loadData() {
   try {
     const response = await fetch('./json.json');
     const jsonData = await response.json(); // Parses the response into a JS object
     console.log(jsonData.questions);
     let questions = jsonData.questions
-  
-  switcher(counter, questions)
+    console.log(jsonData.questions)
+    questCounterBlock.textContent = `${questCounter + 1} / ${questions.length}`
+    switcher(questCounter, questions)
   } catch (error) {
     console.error('Error fetching JSON:', error);
   }
 }
-
-
 loadData();
 function switcher(i, questions) {
-    let answers = questions[i].answers
-      const element = questions[i];
-      console.log(element)
-    voprosBlock.textContent = questions[i].question
-    console.log(answers)
-    for (let j = 0; j < answers.length; j++) {
-         const element = answers[j].answers;
-         const ans1Block = document.createElement('div')
-         ans1Block.classList.add('answer')
-         ans1Block.textContent = element
-         answersBlock.appendChild(ans1Block)
-         ans1Block.addEventListener("click", ()=>{
-          counter++
-          console.log("asdsadas")
-          switcher(i, questions)
-         })
-         console.log(element)
-}
+  answersBlock.textContent = ""
+  let answers = questions[questCounter].answers
+  const element = questions[questCounter];
+  console.log(element)
+  voprosBlock.textContent = questions[questCounter].question
+  console.log(answers)
+  for (let j = 0; j < answers.length; j++) {
+    const element = answers[j].answers;
+    const ans1Block = document.createElement('div')
+    ans1Block.classList.add('answer')
+    ans1Block.textContent = element
+    answersBlock.appendChild(ans1Block)
+    ans1Block.addEventListener("click", () => {
+      console.log(ans1Block.textContent)
+      questCounter++
+      questCounterBlock.textContent = `${questCounter + 1} / ${questions.length}`
+      switcher(i, questions)
+    })
+  }
 }
