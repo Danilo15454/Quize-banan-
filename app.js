6f08e9bb1c5b8ea86db7e002ec7618acd012f64b
peeper4 <mishatronic7045@gmail.com>
2026-01-14 16:48:09 +0200

    g

---
+++ app.js
@@ -0,0 +1,36 @@
+const container = document.querySelector('#cards')
+function createCard({title, text}){
+    const card = document.createElement('div')
+    card.classList.add('card')
+
+    const h3 = document.createElement('h3')
+    h3.classList.add('card_title')
+    h3.textContent = title
+    
+    const p = document.createElement('p')
+    p.classList.add('card_title')
+    p.textContent = text
+   
+    card.appendChild(h3)
+    card.appendChild(p)
+    return card
+    
+} 
+
+
+async function init() {
+    try {
+        const res = await fetch('./data.json')
+        const data = await res.json()
+        data.items.forEach(item => {
+            container.appendChild(createCard(item))
+        });
+    }catch (error){
+        console.error(error)
+    }
+       
+
+}
+
+init()
+
