0a5f68ae80074a6cd60b9fc69002149841366db1
Danila <planset980@gmail.com>
2026-01-28 19:57:49 +0200

    1234123

--- script.js
+++ script.js
@@ -3,6 +3,11 @@ const answersBlock = document.querySelector(".answers")
 const questCounterBlock = document.querySelector(".planka")
 storage = window.localStorage
 let questCounter = 0
+const test = {
+    name: "test",
+    userAnswer: [],
+    userChap: []
+}
 
 async function loadData() {
   try {
@@ -25,6 +30,8 @@ function switcher(i, questions) {
   console.log(element)
   voprosBlock.textContent = questions[questCounter].question
   console.log(answers)
+  const obj = localStorage.getItem("banani")
+  const parse = JSON.parse(obj)
   for (let j = 0; j < answers.length; j++) {
     const element = answers[j].answers;
     const ans1Block = document.createElement('div')
@@ -33,9 +40,12 @@ function switcher(i, questions) {
     answersBlock.appendChild(ans1Block)
     ans1Block.addEventListener("click", () => {
       console.log(ans1Block.textContent)
+      parse.userAnswer[i] = ans1Block.textContent
+      localStorage.setItem("banani", JSON.stringify(parse))
       questCounter++
       questCounterBlock.textContent = `${questCounter + 1} / ${questions.length}`
       switcher(i, questions)
     })
   }
 }
+console.log(storage)
