2fcc1c6b8d0b6b5a53fc56c55417453668c895a1
muha <mishatronic7045@gmail.com>
2026-03-11 16:40:13 +0200

    п

--- project/games/snake.js
+++ project/games/snake.js
@@ -1,84 +1,29 @@
-let rows = document.querySelectorAll("tr")
-let cells = []
+const cells = document.querySelectorAll("td");
+const width = 20;
 
-// создаём массив клеток
-rows.forEach(row=>{
-    cells.push([...row.querySelectorAll("td")])
-})
+cells.forEach((cell, index) => {
 
-let snake = [{x:5,y:5}]
-let dir = "right"
+    const row = Math.floor(index / width);
+    const col = index % width;
 
-// еда
-let food = {
-    x: Math.floor(Math.random()*cells[0].length),
-    y: Math.floor(Math.random()*cells.length)
-}
-
-function draw(){
-
-    // очистить поле
-    cells.forEach(r=>{
-        r.forEach(c=>{
-            c.className=""
-        })
-    })
-
-    // нарисовать змейку
-    snake.forEach(s=>{
-        cells[s.y][s.x].classList.add("snake")
-    })
-
-    // нарисовать еду
-    cells[food.y][food.x].classList.add("food")
-}
-
-function move(){
-
-    let head = {...snake[0]}
-
-    if(dir==="right") head.x++
-    if(dir==="left") head.x--
-    if(dir==="up") head.y--
-    if(dir==="down") head.y++
-
-    // выход за карту
-    if(
-        head.x<0 ||
-        head.y<0 ||
-        head.y>=cells.length ||
-        head.x>=cells[0].length
-    ){
-        alert("Game Over")
-        location.reload()
+    // верхняя стена
+    if (row === 0) {
+        cell.classList.add("wall");
     }
 
-    snake.unshift(head)
-
-    // если съели еду
-    if(head.x===food.x && head.y===food.y){
-
-        food = {
-            x: Math.floor(Math.random()*cells[0].length),
-            y: Math.floor(Math.random()*cells.length)
-        }
-
-    }else{
-        snake.pop()
+    // нижняя стена
+    if (row === width - 1) {
+        cell.classList.add("wall");
     }
 
-    draw()
-}
+    // левая стена
+    if (col === 0) {
+        cell.classList.add("wall");
+    }
 
-// управление
-document.addEventListener("keydown",e=>{
+    // правая стена
+    if (col === width - 1) {
+        cell.classList.add("wall");
+    }
 
-    if(e.key==="ArrowRight") dir="right"
-    if(e.key==="ArrowLeft") dir="left"
-    if(e.key==="ArrowUp") dir="up"
-    if(e.key==="ArrowDown") dir="down"
-
-})
-
-draw()
-setInterval(move,200)
+});
