9ed9caace607eb96b79d31bd62ccbed1fd1fdc66
prior-kratos <artemka140309@gmail.com>
2026-05-25 17:10:59 +0300

    ar

--- pet_food_shop/main.js
+++ pet_food_shop/main.js
@@ -25,3 +25,25 @@ btn.addEventListener('click',function() {
 //     list.appendChild(li);
 //     inp.Value = '';
 // }
+
+// const name = prompt('Whats your name?')
+// alert(`Hello ${name}!`)
+
+// const arr = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20];
+// for (let i = 0; i < arr.length; i++) {
+//     const element = arr[i];
+//     console.log(element);
+// }
+
+// const obj = {
+//     name: 'Jin',
+//     age: 12,
+//     weight: 73
+// }
+//     const name = prompt('Whats your name?')
+//     const age = prompt('How old are you?')
+//     const weight = prompt('Whats your weight?')
+//     obj.name = {name}
+//     obj.age = {age}
+//     obj.weight = {weight}
+//     console.log (obj)
