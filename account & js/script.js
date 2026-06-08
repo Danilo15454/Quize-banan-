21973e63dda4fbeca3356a7b1370b3aade4bb079
prior-kratos <artemka140309@gmail.com>
2026-06-08 17:15:56 +0300

    ar

--- account & js/script.js
+++ account & js/script.js
@@ -82,7 +82,7 @@
 //     const acheck = email.includes('@')
 //     const dotcheck = email.includes('.')
 //     if (!acheck || !dotcheck) {
-//         alert('net @ ili .');
+//         alert('nema @ abo .');
 //         return
 //     };
 //     if (password.length < 8) {
@@ -91,26 +91,33 @@
 //     };
 // });
 
-// const login = 'fhbgfdjgh@ddd.hbgjk';
-// const pass = '';
-// function validatorPass(login, password) {
-//     let login1;
-//     let pass1;
-//     if (login.length !== 0) {
-//         const atsignindex = login.indexOf('@');
-//         const pointindex = login.indexOf('.');
-//         if (
-//             atsignindex !== -1 &&
-//             pointindex !== -1 &&
-//             login.slice(0, atsignindex).length > 0
-//             && login.slice(atsignindex + 1, pointindex).length > 0
-//             && login.slice(pointindex + 1, login.length).length > 0
-//         ) {
-//             login1 = true
-//         } else login1 = false
-//     }
-//     return login1 && pass1 ? true : false;
-// }
-// const result = validatorPass(login, pass);
-// console.log(result);
+const login = "fhbgfdjgh@ddd.hbgjk";
+const pass = "";
+function validatorPass(login, password) {
+    let login1;
+    let pass1;
+    if (login.length !== 0) {
+        const atsignindex = login.indexOf("@");
+        const pointindex = login.indexOf(".");
+        if (
+            atsignindex !== -1 &&
+            pointindex !== -1 &&
+            login.slice(0, atsignindex).length > 0
+            && login.slice(atsignindex + 1, pointindex).length > 0
+            && login.slice(pointindex + 1, login.length).length > 0
+        ){
+            login1 = true
+        } else login1 = false
+    };
+    const hasNum = /\d/.test(password);
+    const uppercase = /[A-Z]/.test(password);
+    const islengthcorrect = password.length >= 8;
+    if (hasNum && uppercase && islengthcorrect) {
+        pass1 = true
+    }else pass1 = false
+
+    return login1 && pass1 ? true : false;
+};
+const result = validatorPass(login, pass);
+console.log(result);
 
